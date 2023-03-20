7f122aed71562227ed44303fae5676840bcf7ba8
Nihal Shrestha <nihal@gurzu.com>
2023-03-20 20:10:00 +0545

    Added function code

--- app/src/main/java/com/nepsian/emptyactivity/MainActivity3.java
+++ app/src/main/java/com/nepsian/emptyactivity/MainActivity3.java
@@ -3,6 +3,10 @@ package com.nepsian.emptyactivity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity3 extends AppCompatActivity {
 
@@ -10,5 +14,14 @@ public class MainActivity3 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+
+        Button btn = findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("Button: ", "Button has been clicked!!!!!!!!!!!!!!!!");
+                Toast.makeText(getApplicationContext(), "hello world", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
