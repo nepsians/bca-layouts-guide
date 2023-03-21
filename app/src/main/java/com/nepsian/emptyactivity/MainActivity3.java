d8d34831288a8902157603327370aadc392e6771
Nihal Shrestha <nihal@gurzu.com>
2023-03-21 20:23:04 +0545

    Added snackbar and toast example

--- app/src/main/java/com/nepsian/emptyactivity/MainActivity3.java
+++ app/src/main/java/com/nepsian/emptyactivity/MainActivity3.java
@@ -8,19 +8,24 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.android.material.snackbar.Snackbar;
+
 public class MainActivity3 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+        Log.d("Calling from Activity:", "onCreate----LifeCycle");
 
-        Button btn = findViewById(R.id.button);
-        btn.setOnClickListener(new View.OnClickListener() {
+        Button btn = findViewById(R.id.clickMeButton);
+
+        btn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-                Log.d("Button: ", "Button has been clicked!!!!!!!!!!!!!!!!");
-                Toast.makeText(getApplicationContext(), "hello world", Toast.LENGTH_LONG).show();
+                Snackbar.make(view, "Button has been clicked!!!!!", Snackbar.LENGTH_SHORT).show();
+//                Toast.makeText(getApplicationContext(), "Button has been clicked!!!!!", Toast.LENGTH_LONG).show();
+
             }
         });
     }
